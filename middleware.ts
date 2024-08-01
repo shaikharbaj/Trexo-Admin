5c65cc1be074bc6bee06f556a46a410717a012d3
Azhar Pathan <apathan@katalysttech.com>
2024-08-01 18:52:52 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { menusConfig } from "./config/menus";
 
 const privateRoutes = [
   '/dashboard',
@@ -10,12 +11,30 @@ const publicRoutes = [
   '/forgot-password'
 ];
 
+// Function to recursively find all href values
+function findHrefs(navArray:any) {
+  let hrefs = <any>[];
+
+  navArray.forEach((item:any) => {
+    if (item.href) {
+      hrefs.push(item.href);
+    }
+    if (item.child) {
+      hrefs = hrefs.concat(findHrefs(item.child));
+    }
+  });
+
+  return hrefs;
+}
+
+const hrefs = findHrefs(menusConfig.mainNav);
+
 export function middleware(request: any) {
   const url = request.nextUrl.clone();
   const pathname = request.nextUrl.pathname; // Check if there is any supported locale in the pathname
   const token = request.cookies.get("token")?.value;
   if (!token) {
-    if (privateRoutes.includes(pathname)) {
+    if (hrefs.includes(pathname)) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
   } else if (publicRoutes.includes(pathname)) {
