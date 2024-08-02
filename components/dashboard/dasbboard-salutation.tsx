63115bf4343ae375a00cf9e6f880091adc81cffa
Sohel Patel <spatel1@katalysttech.com>
2024-08-02 16:21:16 +0530

    Dashboard layout chnage

--- components/dashboard/dasbboard-salutation.tsx
+++ components/dashboard/dasbboard-salutation.tsx
@@ -3,8 +3,10 @@ import { RootState } from "@/redux/store";
 import { useAppSelector } from "@/hooks";
 import { dateWithDayNameDayMonthNameAndYear } from "@/utils/date";
 
-const Salutation = () => {
-    const {isProfileLoading, profile} = useAppSelector((state: RootState) => state.profile);
+const DashboardSalutation = () => {
+  const { isProfileLoading, profile } = useAppSelector(
+    (state: RootState) => state.profile
+  );
   return (
     <div className=" space-y-2">
       <div className=" font-light text-2xl">
@@ -12,12 +14,18 @@ const Salutation = () => {
         Welcome Back,{" "}
         <span className="font-semibold">
           {" "}
-          <span>{profile?.first_name} {profile?.last_name}</span>!
+          <span>
+            {profile?.first_name} {profile?.last_name}
+          </span>
+          !
         </span>
       </div>
-      <div className=" text-xs opacity-80"> {dateWithDayNameDayMonthNameAndYear()}</div>
+      <div className=" text-xs opacity-80">
+        {" "}
+        {dateWithDayNameDayMonthNameAndYear()}
+      </div>
     </div>
   );
 };
 
-export default Salutation;
+export default DashboardSalutation;
