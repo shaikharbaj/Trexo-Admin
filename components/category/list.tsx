1f01b3d8fa3e981b3b17cf66797ecc602fa9dcce
Sohel Patel <spatel1@katalysttech.com>
2024-08-06 16:58:58 +0530

    Chnages

--- components/category/list.tsx
+++ components/category/list.tsx
@@ -20,13 +20,13 @@ const CategoryList: React.FunctionComponent<ICategoryProps> = () => {
                 <Breadcrumbs>
                     <BreadcrumbItem href="/dashboard">{t('Dashboard')}</BreadcrumbItem>
                     <BreadcrumbItem>{t('Master')}</BreadcrumbItem>
-                    <BreadcrumbItem>{t('Catyegory')}</BreadcrumbItem>
+                    <BreadcrumbItem>{t('Category')}</BreadcrumbItem>
                 </Breadcrumbs>
             </div>
             <div className="space-y-6">
                 <div className="flex items-center flex-wrap justify-between gap-4">
                     <div className="text-2xl font-semibold text-default-800 pb-2 relative after:absolute after:h-0.5 after:rounded-md after:w-11 after:bg-primary after:left-0 after:bottom-0">
-                    {t('Catyegory')}
+                    {t('Category')}
                     </div>
                     <div className="flex-none flex items-center justify-end gap-4">
                         <Button onClick={() => setModalIsOpen(true)}>
