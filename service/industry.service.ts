1c5e0c72bf8d76bf9bacf815a87fcf6934a14167
Azhar Pathan <apathan@katalysttech.com>
2024-08-06 11:27:57 +0530

    updated

--- service/industry.service.ts
+++ service/industry.service.ts
@@ -1,24 +1,6 @@
 import { refreshData } from "@/redux/slice/datatable.slice";
-import { setPagination } from "@/redux/slice/paginate.slice";
 import { store } from "@/redux/store";
-import { createIndustryThunk, deleteIndustryThunk, fetchIndustryByIdThunk, industryListThunk, updateIndustryThunk } from "@/redux/thunk/industry.thunk";
-
-
-//Function to fetch industry list with filter
-export const fetchIndustryWithFilter = async (listPayload: any) => {
-  try {
-    const { payload } = await store.dispatch(industryListThunk(listPayload));
-    if (payload?.status !== true) {
-      return { status: payload?.status, statusCode: payload?.statusCode, message: payload?.message };
-    }
-    store.dispatch(setPagination(payload?.data?.meta));
-    return { status: payload?.status, statusCode: payload?.statusCode, message: payload?.message };
-  } catch (error: any) {
-    throw new Error(
-      error.response?.data?.message || "Something went wrong."
-    );
-  }
-};
+import { createIndustryThunk, deleteIndustryThunk, fetchIndustryByIdThunk, updateIndustryThunk } from "@/redux/thunk/industry.thunk";
 
 //Function to create industry
 export const createIndustry = async (createPayload: any) => {
