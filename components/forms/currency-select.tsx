7c7f3407dd61127c2cf2b7eee5c1e98aeda12286
prasadmore01 <prasadmoremain@gmail.com>
2024-08-07 19:38:44 +0530

    Integrated api in social media module

--- components/forms/currency-select.tsx
+++ components/forms/currency-select.tsx
@@ -1,33 +1,34 @@
 "use client"
 import { Icon } from "@iconify/react";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  FormSelect,
+  FormSelectContent,
+  FormSelectItem,
+  FormSelectTrigger,
+  FormSelectValue,
+} from "@/components/ui/select-react-hook-form";
 import { UseFormRegister } from "react-hook-form";
 
 interface CurrencySelectProps {
   register: UseFormRegister<any>;
+  currencyValue: any;
 }
 
-const CurrencySelect: React.FC<CurrencySelectProps> = ({ register }) => {
+const CurrencySelect: React.FC<CurrencySelectProps> = ({ register, currencyValue }) => {
   return (
-    <Select {...register("currency_symbol")}>
-      <SelectTrigger>
-        <SelectValue placeholder="Select One" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="RUPEE"><Icon icon="heroicons:currency-rupee" className="w-5 h-5 inline-block" /> Rupee</SelectItem>
-        <SelectItem value="DOLLAR"><Icon icon="heroicons:currency-dollar" className="w-5 h-5 inline-block" /> Dollar</SelectItem>
-        <SelectItem value="EURO"><Icon icon="heroicons:currency-euro" className="w-5 h-5 inline-block" /> Euro</SelectItem>
-        <SelectItem value="POUND"><Icon icon="heroicons:currency-pound" className="w-5 h-5 inline-block" /> Pound</SelectItem>
-        <SelectItem value="YEN"><Icon icon="heroicons:currency-yen" className="w-5 h-5 inline-block" /> Yen</SelectItem>
-        <SelectItem value="BANGLADESHI"><Icon icon="heroicons:currency-bangladeshi" className="w-5 h-5 inline-block" /> Bangladeshi</SelectItem>
-      </SelectContent>
-    </Select>
+    <FormSelect {...register("currency_symbol")} value={currencyValue}>
+      <FormSelectTrigger>
+        <FormSelectValue placeholder="Select One" />
+      </FormSelectTrigger>
+      <FormSelectContent>
+        <FormSelectItem value="RUPEE"><Icon icon="heroicons:currency-rupee" className="w-5 h-5 inline-block" /> Rupee</FormSelectItem>
+        <FormSelectItem value="DOLLAR"><Icon icon="heroicons:currency-dollar" className="w-5 h-5 inline-block" /> Dollar</FormSelectItem>
+        <FormSelectItem value="EURO"><Icon icon="heroicons:currency-euro" className="w-5 h-5 inline-block" /> Euro</FormSelectItem>
+        <FormSelectItem value="POUND"><Icon icon="heroicons:currency-pound" className="w-5 h-5 inline-block" /> Pound</FormSelectItem>
+        <FormSelectItem value="YEN"><Icon icon="heroicons:currency-yen" className="w-5 h-5 inline-block" /> Yen</FormSelectItem>
+        <FormSelectItem value="BANGLADESHI"><Icon icon="heroicons:currency-bangladeshi" className="w-5 h-5 inline-block" /> Bangladeshi</FormSelectItem>
+      </FormSelectContent>
+    </FormSelect>
   );
 };
 
--- service/global-setting.service.ts
+++ service/global-setting.service.ts
@@ -1,20 +1,35 @@
 import { refreshData } from "@/redux/slice/datatable.slice";
 import { store } from "@/redux/store";
-import { createGlobalSettingThunk } from "@/redux/thunk/global-setting.thunk";
+import { createGlobalSettingThunk, globalSettingListThunk } from "@/redux/thunk/global-setting.thunk";
 
 
+//Function to create global setting
 export const createGlobalSetting = async (createPayload: any) => {
     try {
         const { payload } = await store.dispatch(createGlobalSettingThunk(createPayload));
         if (payload?.status !== true) {
             return { status: payload?.status, statusCode: payload?.statusCode, message: payload?.message };
         }
-        store.dispatch(refreshData());
         return { status: payload?.status, statusCode: payload?.statusCode, message: payload?.message };
     } catch (error: any) {
         throw new Error(
             error.response?.data?.message || "Something went wrong."
         );
     }
+}
 
+
+//Function to fetch global setting
+export const fetchGlobalSetting = async () => {
+    try {
+        const { payload } = await store.dispatch(globalSettingListThunk());
+        if (payload?.status !== true) {
+            return { status: payload?.status, statusCode: payload?.statusCode, message: payload?.message };
+        }
+        return { status: payload?.status, statusCode: payload?.statusCode, message: payload?.message, data: payload?.data };
+    } catch (error: any) {
+        throw new Error(
+            error.response?.data?.message || "Something went wrong."
+        );
+    }
 }
