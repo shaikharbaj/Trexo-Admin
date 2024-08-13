5f556570bb34114c05542d1e51bb3e7b40cf15b0
Arbaj Shaikh <shaikharbaj2001@gmail.com>
2024-08-13 18:11:28 +0530

    remove unwanted code

--- components/contact-us/list.tsx
+++ components/contact-us/list.tsx
@@ -1,12 +1,8 @@
 "use client";
 import React from "react";
 import { useTranslations } from "next-intl";
-import { Icon } from "@iconify/react";
 import { Breadcrumbs, BreadcrumbItem } from "@/components/ui/breadcrumbs";
 import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { CreateUpdateCountryModal } from "@/components/modals";
-import { CountryTable } from "@/components/tables";
 import { openPopup } from "@/service/modal.service";
 import { useAppSelector } from "@/hooks";
 import { RootState } from "@/redux/store";
@@ -35,16 +31,6 @@ const ContactUsList: React.FunctionComponent<IContactUsProps> = () => {
           <div className="text-2xl font-semibold text-default-800 pb-2 relative after:absolute after:h-0.5 after:rounded-md after:w-11 after:bg-primary after:left-0 after:bottom-0">
             {t("ContactUs")}
           </div>
-          {/* <div className="flex-none flex items-center justify-end gap-4">
-            <Button onClick={handleOpenModal}>
-              <Icon
-                icon="heroicons:plus"
-                className="w-5 h-5 ltr:mr-2 rtl:ml-2"
-              />
-              {t("Add New")}
-            </Button>
-            <CreateUpdateContactUsModal trans={t} />
-          </div> */}
         </div>
       </div>
       <div className="grid grid-cols-1 gap-6">
